9dde78b944369924f5a051a35f90dcd3c6dfd54a
Vid <vid@nearlythreethings.com>
2024-06-09 14:29:58 -0700

    adding new nested feed elements

--- feed.py
+++ feed.py
@@ -21,6 +21,19 @@ with open('feed.yaml', 'r') as file:
   xml_tree.SubElement(channel_element, 'link').text = link_prefix
   xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
 
+  for item in yaml_data['item']:
+    item_element = xml_tree.SubElement(channel_element, 'item')
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text = item['author']
+    xml_tree.SubElement(item_element, 'description').text = item['description']
+    xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
+    xml_tree.SubElement(item_element, 'pubdate').text = item['published']
+
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+      'url': link_prefix + item['file'],
+      'type': 'audio/mpeg',
+      'length': item['length']
+    })
 
   output_tree = xml_tree.ElementTree(rss_element)
   output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
