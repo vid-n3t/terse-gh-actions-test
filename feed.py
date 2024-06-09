b355a94b1c542c82ae370d888dd35e2a4623b982
Vid <vid@nearlythreethings.com>
2024-06-09 14:22:15 -0700

    add link, img, cat to feed

--- feed.py
+++ feed.py
@@ -9,16 +9,19 @@ with open('feed.yaml', 'r') as file:
     'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
   
   channel_element = xml_tree.SubElement(rss_element, 'channel')
-  # link_prefix = yaml_data('link')
+  link_prefix = yaml_data('link')
 
   xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
-xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
-xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
-xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['itunes:author']
-xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-xml_tree.SubElement(channel_element, 'itunes:image').text = yaml_data['itunes:image']
-xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+  xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+  xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+  xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['itunes:author']
+  xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
+  xml_tree.SubElement(channel_element, 'itunes:image', {'href', link_prefix + yaml_data['image']})
+  xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+  xml_tree.SubElement(channel_element, 'link').text = link_prefix
+  xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
 
-output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
+
+  output_tree = xml_tree.ElementTree(rss_element)
+  output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
   
